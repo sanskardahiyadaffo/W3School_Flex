7deb6295f050cc6e2fb3136079a235993c5351c9
sanskardahiya <sanskar.dahiya@daffodilsw.com>
2020-02-10 14:20:09 +0530

    abc

---
+++ js.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>W3School</title>
+</head>
+<body>
+<div class="Main">
+  <div class="Header">
+    <div class="Header1">Header1</div>
+    <div class="Header2">Header2</div>  
+  </div>
+  <div class="Body">BodySection</div>
+</div>
+</body>
+</html>
